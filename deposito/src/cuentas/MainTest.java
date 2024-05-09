215b80bba9a94ef712383903ebac3a71d8085766
dadua <dadua@DESKTOP-EUNQJ68>
2024-05-09 18:20:04 +0200

    commit

---
+++ deposito/src/cuentas/MainTest.java
@@ -0,0 +1,14 @@
+package cuentas;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
